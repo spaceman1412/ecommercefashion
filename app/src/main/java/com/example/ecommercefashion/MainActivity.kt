152eb188549ba6d95c1ae0b2016d584a3410a9e4
spaceman1412 <spacemannormal@gmail.com>
2021-09-12 10:34:59 +0700

    Finish 2 UI and basic navigate

--- app/src/main/java/com/example/ecommercefashion/MainActivity.kt
+++ app/src/main/java/com/example/ecommercefashion/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.ecommercefashion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val click : LinearLayout = findViewById(R.id.linear_click)
+        click.setOnClickListener{
+            val intent = Intent(this,ItemDetail::class.java)
+            startActivity(intent)
+        }
     }
 }
