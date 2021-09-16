aa0742acb7b2edbb0dadfdfb4eed4439302eebf4
spaceman1412 <spacemannormal@gmail.com>
2021-09-16 20:16:18 +0700

    Set up login screen

---
+++ app/src/main/java/com/example/ecommercefashion/LoginSreen.kt
@@ -0,0 +1,13 @@
+package com.example.ecommercefashion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginSreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_sreen)
+
+    }
+}
---
+++ app/src/main/java/com/example/ecommercefashion/RegisterFragment.kt
@@ -0,0 +1,29 @@
+package com.example.ecommercefashion
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class RegisterFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_register, container, false)
+
+
+
+        return view
+    }
+
+
+}
