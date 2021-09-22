1eb2a779736909d8293e2aa5cc371737f0f2e37b
spaceman1412 <spacemannormal@gmail.com>
2021-09-22 21:30:28 +0700

    Fix navigate login screen and main acitivy

--- app/src/main/java/com/example/ecommercefashion/MainActivity.kt
+++ app/src/main/java/com/example/ecommercefashion/MainActivity.kt
@@ -3,17 +3,41 @@ package com.example.ecommercefashion
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import android.widget.LinearLayout
+import com.example.ecommercefashion.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        Log.d("MainActivity","The current user is ${currentUser?.uid.toString()}")
+        if(currentUser == null) {
+            val intent = Intent(this,LoginSreen::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
 
 
-        val click : LinearLayout = findViewById(R.id.linear_click)
+        val click : LinearLayout = binding.linearClick
         click.setOnClickListener{
             val intent = Intent(this,ShoppingCartActivity::class.java)
+            startActivity(intent)
+        }
+
+        val icon : ImageView = binding.searchIconMainActivity
+        icon.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this,LoginSreen::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
         }
     }
