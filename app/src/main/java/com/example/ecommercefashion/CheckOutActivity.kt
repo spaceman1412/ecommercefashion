f36b31194af0b43db9790062632a763432899e35
spaceman1412 <spacemannormal@gmail.com>
2021-09-27 21:45:25 +0700

    Finish read from firebase database

--- app/src/main/java/com/example/ecommercefashion/CheckOutActivity.kt
+++ app/src/main/java/com/example/ecommercefashion/CheckOutActivity.kt
@@ -2,7 +2,17 @@ package com.example.ecommercefashion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 import com.example.ecommercefashion.databinding.ActivityCheckOutBinding
+import com.example.ecommercefashion.databinding.ItemCheckOutBinding
+import com.example.ecommercefashion.models.ItemCart
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
@@ -21,19 +31,48 @@ class CheckOutActivity : AppCompatActivity() {
         val adapter = GroupAdapter<GroupieViewHolder>()
         binding.recyclerViewCheckOut.adapter = adapter
 
-        adapter.add(CheckOutItem())
-        adapter.add(CheckOutItem())
-        adapter.add(CheckOutItem())
+//        adapter.add(CheckOutItem())
+//        adapter.add(CheckOutItem())
+//        adapter.add(CheckOutItem())
+
+        val uid = FirebaseAuth.getInstance().uid
+
+        val ref = FirebaseDatabase.getInstance().getReference("/cart/$uid")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                snapshot.children.forEach {
+                    val shopItem = it.getValue(ItemCart::class.java)
+
+                    if (shopItem != null) {
+                        adapter.add(CheckOutItem(shopItem))
+//                        price += shopItem.price
+                        Log.d("CheckOutActivity", "Added to adapter ${shopItem.id}")
+                    }
+                }
+//                binding.priceTextViewActivityShoppingCart.text = "$$price"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d(ShoppingCartActivity.TAG, "Failed to adapter")
+            }
+        })
     }
 }
 
-class CheckOutItem : Item<GroupieViewHolder>(){
-    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-
+class CheckOutItem(val item: ItemCart) : BindableItem<ItemCheckOutBinding>(){
+    override fun bind(viewBinding: ItemCheckOutBinding, position: Int) {
+        viewBinding.titleNameTextViewItemCheckOut.text = item.name
+        viewBinding.primaryImageImageViewCheckOut.setImageResource(item.primaryImage)
+        viewBinding.priceTextViewItemCheckOut.text = "$${item.price}"
     }
 
     override fun getLayout(): Int {
         return R.layout.item_check_out
     }
+
+    override fun initializeViewBinding(view: View): ItemCheckOutBinding {
+        return ItemCheckOutBinding.bind(view)
+    }
+
 
 }
