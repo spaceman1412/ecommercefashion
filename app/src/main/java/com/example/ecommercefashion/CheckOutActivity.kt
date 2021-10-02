c5a91500b01a8df58546398a2c2e9a94c81eef94
spaceman1412 <spacemannormal@gmail.com>
2021-10-02 23:36:33 +0700

    Finish checkout screen (not fix ui yet)

--- app/src/main/java/com/example/ecommercefashion/CheckOutActivity.kt
+++ app/src/main/java/com/example/ecommercefashion/CheckOutActivity.kt
@@ -4,11 +4,14 @@ import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.ecommercefashion.databinding.ActivityCheckOutBinding
-import com.example.ecommercefashion.databinding.ItemCheckOutBinding
+import com.example.ecommercefashion.databinding.ItemCheckOutListBinding
 import com.example.ecommercefashion.models.ItemCart
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
@@ -16,6 +19,7 @@ import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import com.xwray.groupie.viewbinding.BindableItem
+import java.sql.Array
 
 class CheckOutActivity : AppCompatActivity() {
 
@@ -24,8 +28,12 @@ class CheckOutActivity : AppCompatActivity() {
         val TAG = "CheckOutActivity"
     }
 
+    val adapter = GroupAdapter<GroupieViewHolder>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         binding = ActivityCheckOutBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
@@ -35,7 +43,10 @@ class CheckOutActivity : AppCompatActivity() {
         fetchCheckOutDatabase()
 
         fetchCheckOutList()
-
+        val recyclerView : RecyclerView = binding.recyclerViewCheckOut
+        recyclerView.adapter = adapter
+//        adapter.add(CheckOutItemList())
+//        adapter.add(CheckOutItemList())
 
 
 //        val child : View = layoutInflater.inflate(R.layout.item_check_out,null)
@@ -56,6 +67,10 @@ class CheckOutActivity : AppCompatActivity() {
 
         listOfListItemCart.values.forEach {
             Log.d(TAG,it.toString())
+            adapter.add(CheckOutItemList(layoutInflater,it))
+//            val linearLayout : LinearLayout = binding.linearLayoutCheckOut
+//            val child : View = layoutInflater.inflate(R.layout.item_check_out,null)
+//            linearLayout.addView(child)
         }
     }
 
@@ -120,18 +135,25 @@ class CheckOutActivity : AppCompatActivity() {
     }
 }
 
-//class CheckOutItem(val item: ItemCart) : BindableItem<ItemCheckOutBinding>(){
-//    override fun bind(viewBinding: ItemCheckOutBinding, position: Int) {
-//        viewBinding.titleNameTextViewItemCheckOut.text = item.name
-//        viewBinding.primaryImageImageViewCheckOut.setImageResource(item.primaryImage)
-//        viewBinding.priceTextViewItemCheckOut.text = "$${item.price}"
-//    }
-//
-//    override fun getLayout(): Int {
-//        return R.layout.item_check_out
-//    }
-//
-//    override fun initializeViewBinding(view: View): ItemCheckOutBinding {
-//        return ItemCheckOutBinding.bind(view)
-//    }
-//}
+class CheckOutItemList(layoutInflater: LayoutInflater,listItem : ArrayList<ItemCart?>) : BindableItem<ItemCheckOutListBinding>(){
+    val layoutInflater : LayoutInflater = layoutInflater
+    val listOfItemCart : ArrayList<ItemCart?> = listItem
+    override fun bind(viewBinding: ItemCheckOutListBinding, position: Int) {
+        listOfItemCart.forEach {
+            val child : View = layoutInflater.inflate(R.layout.item_check_out,null)
+            child.findViewById<TextView>(R.id.titleName_textView_itemCheckOut).text = it?.name
+            child.findViewById<TextView>(R.id.price_textView_itemCheckOut).text = "$${it?.price}"
+            child.findViewById<ImageView>(R.id.primaryImage_imageView_itemcheckOut).setImageResource(
+                it!!.primaryImage)
+            viewBinding.linearLayoutCheckOut.addView(child)
+        }
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.item_check_out_list
+    }
+
+    override fun initializeViewBinding(view: View): ItemCheckOutListBinding {
+        return ItemCheckOutListBinding.bind(view)
+    }
+}
